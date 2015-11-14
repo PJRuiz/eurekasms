b838881cc9c4db49225124a3c2bda01a04ccb755
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-14 15:37:05 +0900

    Add static assets

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -105,6 +105,15 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # if ON_HEROKU == '1':
     # Parse database configuration from $DATABASE_URL
