5bb7318bfb45fdbf5eb445f6ac41af60d2bae3e3
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-15 16:10:19 +0900

    Create Subscribe View

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marketing'
+    'marketing',
+    'subscribers',
 )
 
 MIDDLEWARE_CLASSES = (
