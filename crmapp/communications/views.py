1deee07e39626793e37866e44039d976b9e607b6
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-18 02:33:49 +0900

    create comm detail view

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+
+from .models import Communication
+
+@login_required()
+def comm_detail(request, uuid):
+
+    comm = Communication.objects.get(uuid=uuid)
+    if comm.owner != request.user:
+            return HttpResponseForbidden()
+
+    return render(request, 'communications/comm_detail.html', {'comm':comm})
