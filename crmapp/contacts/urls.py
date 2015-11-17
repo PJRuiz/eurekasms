d2745c4341a1acfc81045695cd1330c2945d0cc8
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-17 17:43:06 +0900

    Update urls

--- crmapp/contacts/urls.py
+++ crmapp/contacts/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
 
 contact_urls = patterns('',
-
-    url(r'^$', 'crmapp.contacts.views.contact_detail', name="contact_detail"),
+	url(r'^contact/new/$',
+    	'crmapp.contacts.views.contact_cru', name='contact_new'
+	),
+    url(r'^$', 
+    	'crmapp.contacts.views.contact_detail', name="contact_detail"
+    ),
 
 )
