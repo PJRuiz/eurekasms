f1bb663a6edea44d659ab86ce55dc442e23acae1
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-17 21:10:56 +0900

    Update to allow editing contacts

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -7,6 +7,10 @@ from django.http import HttpResponseForbidden
 
 from django.core.urlresolvers import reverse
 
+from django.shortcuts import get_object_or_404
+
+from crmapp.accounts.models import Account 
+
 from .forms import ContactForm
 
 
@@ -24,10 +28,16 @@ def contact_detail(request, uuid):
     )
 
 @login_required()
-def contact_cru(request):
+def contact_cru(request, uuid=None, account=None):
+	if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        contact = Contact(owner=request.user)
 
     if request.POST:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -43,11 +53,22 @@ def contact_cru(request):
                 args=(account.uuid,)
             )
             return HttpResponseRedirect(reverse_url)
+    	else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
+
     else:
-        form = ContactForm()
+        form = ContactForm(instance=contact)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
+
 
     variables = {
         'form': form,
+        'contact': contact,
+        'account': account
     }
 
     template = 'contacts/contact_cru.html'
