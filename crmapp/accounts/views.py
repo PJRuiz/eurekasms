9c5b85c7f44b42b2b5bfa1f93074964b4ebd84f2
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-17 13:32:20 +0900

    Moved accounts app inside crmapp to fix bug

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -4,6 +4,11 @@ from django.utils.decorators import method_decorator
 from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect 
+from django.core.urlresolvers import reverse
+
+from .forms import AccountForm
+
 from .models import Account
 
 class AccountList(ListView):
@@ -42,3 +47,28 @@ def account_detail(request, uuid):
     }
 
     return render(request, 'accounts/account_detail.html', variables)
+
+@login_required()
+def account_cru(request):
+
+    if request.POST:
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.owner = request.user
+            account.save()
+            redirect_url = reverse(
+                'crmapp.accounts.views.account_detail',
+                args=(account.uuid,)
+            )
+            return HttpResponseRedirect(redirect_url)
+    else:
+        form = AccountForm()
+
+    variables = {
+        'form': form,
+    }
+
+    template = 'accounts/account_cru.html'
+
+    return render(request, template, variables)
--- crmapp/settings.py
+++ crmapp/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'marketing',
     'subscribers',
-    'accounts',
+    'crmapp.accounts',
 )
 
 MIDDLEWARE_CLASSES = (
