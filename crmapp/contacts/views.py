f9325625330e5c3abe7d782f398836139c7df1ca
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-17 17:47:29 +0900

    create new account view

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+
 from django.contrib.auth.decorators import login_required
+
+from django.http import HttpResponseRedirect
+from django.http import HttpResponseForbidden
+
+from django.core.urlresolvers import reverse
+
+from .forms import ContactForm
+
 
 from .models import Contact
 
@@ -13,3 +22,34 @@ def contact_detail(request, uuid):
                 'contacts/contact_detail.html', 
                 {'contact': contact}
     )
+
+@login_required()
+def contact_cru(request):
+
+    if request.POST:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # make sure the user owns the account
+            account = form.cleaned_data['account']
+            if account.owner != request.user:
+                return HttpResponseForbidden()
+            # save the data
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
+            # return the user to the account detail view
+            reverse_url = reverse(
+                'crmapp.accounts.views.account_detail',
+                args=(account.uuid,)
+            )
+            return HttpResponseRedirect(reverse_url)
+    else:
+        form = ContactForm()
+
+    variables = {
+        'form': form,
+    }
+
+    template = 'contacts/contact_cru.html'
+
+    return render(request, template, variables)
