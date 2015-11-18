77e9c6c6112c59fe77d331f5133e9844c98c4db3
Pedro Ruiz <pedro.ruiz.diaz@me.com>
2015-11-18 17:44:51 +0900

    communication edit links

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.decorators import login_required
+
 from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
+
 from django.core.urlresolvers import reverse
+
+from crmapp.accounts.models import Account
 
 from .forms import CommunicationForm  
 
@@ -19,30 +24,44 @@ def comm_detail(request, uuid):
     return render(request, 'communications/comm_detail.html', {'comm':comm})
 
 @login_required()
-def comm_cru(request):
+def comm_cru(request, uuid=None, account=None):
+
+    if uuid:
+        comm = get_object_or_404(Communication, uuid=uuid)
+        if comm.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        comm = Communication(owner=request.user)
 
     if request.POST:
-        form = CommunicationForm(request.POST)
+        form = CommunicationForm(request.POST, instance=comm)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
             if account.owner != request.user:
                 return HttpResponseForbidden()
             # save the data
-            comm = form.save(commit=False)
-            comm.owner = request.user
-            comm.save()
+            form.save()
             # return the user to the account detail view
             reverse_url = reverse(
-                'crmapp.accounts.views.account_detail',
+                'crmapp.contacts.views.account_detail',
                 args=(account.uuid,)
             )
             return HttpResponseRedirect(reverse_url)
+        else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
     else:
-        form = CommunicationForm()
+        form = CommunicationForm(instance=comm)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'comm':comm,
+        'account': account
     }
 
     template = 'communications/comm_cru.html'
